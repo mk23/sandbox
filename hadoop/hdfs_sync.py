37be9047d9e831bd50ace84bf84a8184321b6cd7
Max Kalika <mkalika@apple.com>
2015-05-11 08:44:51 -0700

    Use webhdfs and parallelize hdfs_sync.

--- hadoop/hdfs_sync.py
+++ hadoop/hdfs_sync.py
@@ -1,96 +1,158 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
-import collections
+import argparse
 import datetime
 import errno
-import hashlib
+import getpass
+import multiprocessing.pool
 import os
 import pickle
 import shutil
 import socket
+import stat
 import subprocess
 import sys
 import syslog
 import tempfile
 import traceback
 import urlparse
+import webhdfs
 
-HDFS_File = collections.namedtuple('HDFS_File', 'mode repl owner group bytes date time url')
-class Sync_File:
-    def __init__(self, item, cmd, src, tgt):
-        if type(item) == list:
-            self.hdfs = HDFS_File(item)
-        elif type(item) == str and item:
-            self.hdfs = HDFS_File(*(item.strip().split(None, len(HDFS_File._fields))))
-        else:
-            raise TypeError
 
-        url = urlparse.urlparse(src)
+class SyncFile(object):
+    _archive_suffixes = {
+        '.zip':     'unzip',
+        '.txz':     'tarxz',
+        '.tar.xz':  'tarxz',
+        '.tgz':     'targz',
+        '.tar.gz':  'targz',
+        '.tbz2':    'tarbz2',
+        '.tar.bz2': 'tarbz2',
+    }
+    _archive_commands = {
+        'unzip':  ['unzip', '-qq'],
+        'targz':  ['tar', '-xzf'],
+        'tarxz':  ['tar', '-xJf'],
+        'tarbz2': ['tar', '-xjf'],
+    }
 
-        self.name = self.hdfs.url[len(src) if self.hdfs.url.startswith(src) else len(url.path):]
+    def __init__(self, remote, source, mirror, unpack):
+        self.remote = remote
+        self.source = source
+        self.mirror = mirror
+        self.unpack = unpack
 
-        self.src = '%s://%s' % (url.scheme, os.path.normpath('%s/%s/%s' % (url.netloc, url.path, self.name)))
-        self.tgt = os.path.normpath('%s/%s' % (tgt, self.name))
-        self.cmd = cmd
+    @property
+    def fullname(self):
+        return self.mirror + self.remote.full[len(self.source):]
 
-    def __str__(self):
-        return '%s: %s' % (self.name, ' '.join(list(self.hdfs)))
+    @property
+    def zip_path(self):
+        for e, t in self._archive_suffixes.items():
+            if self.fullname.endswith(e):
+                return self.unpack + self.remote.full[len(self.source):-len(e)]
 
-    def isdir(self):
-        return self.hdfs.mode.startswith('d')
+    @property
+    def zip_exec(self):
+        for e, t in self._archive_suffixes.items():
+            if self.fullname.endswith(e):
+                return self._archive_commands[t]
+
+    @property
+    def modified(self):
+        return not os.path.exists(self.fullname) or getattr(self, 'mtime', 0) < os.stat(self.fullname).st_mtime
 
     def equal(self, item):
-        return self.hdfs == item.hdfs
+        return self.remote.size == item.remote.size and self.remote.date == item.remote.date
 
-    def mkmd5(self):
-        md5 = hashlib.md5()
-        with open(self.tgt, 'rb') as f:
-            for part in iter(lambda: f.read(128 * md5.block_size), b''):
-                md5.update(part)
-
-        return md5.hexdigest()
-
-    def mkdir(self, path):
+    def mkdir(self, path=None):
         try:
             os.makedirs(path)
+            syslog.syslog(syslog.LOG_INFO, 'created local directory: %s' % path)
         except OSError as e:
             if e.errno != errno.EEXIST or not os.path.exists(path):
                 raise
 
-    def fetch(self, temp=tempfile.gettempdir(), skip=False):
-        cmd = {'old': 'hadoop fs', 'new': 'hdfs dfs'}[self.cmd].split()
-        cmd.extend(['-get', self.src, temp])
+    def rmdir(self, path):
+        limit = max(os.path.commonprefix(i) for i in [[self.mirror, path], [self.unpack, path]])
 
-        syslog.syslog(syslog.LOG_DEBUG, 'running: %s' % ' '.join(cmd))
-        try:
-            if not skip:
-                temp = self.tgt.replace(os.path.dirname(self.tgt), temp)
-                subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        if not limit:
+            syslog.syslog(syslog.LOG_ERR, 'request to purge untracked path: %s' % path)
 
-                self.mkdir(os.path.dirname(self.tgt))
-                os.rename(temp, self.tgt)
+        while True:
+            path = os.path.dirname(path)
+            if path == limit:
+                break
 
-                self.md5 = self.mkmd5()
-
-            syslog.syslog(syslog.LOG_INFO, '%s: fetched hdfs file from %s' % (self.tgt, self.hdfs.url))
-            return True
-        except subprocess.CalledProcessError as e:
-            syslog.syslog(syslog.LOG_ERR, e.output)
-        except Exception as e:
-            log_exception(e)
-
-        return False
+            try:
+                os.rmdir(path)
+                syslog.syslog(syslog.LOG_INFO, 'purged local empty directory: %s' % path)
+            except OSError as e:
+                if e.errno == errno.ENOTEMPTY:
+                    break
+                else:
+                    raise
 
     def purge(self, skip=False):
-        try:
-            if not skip:
-                os.unlink(self.tgt)
-            syslog.syslog(syslog.LOG_INFO, 'purged local file: %s' % self.tgt)
+        if skip:
             return True
+
+        try:
+            if self.zip_path:
+                shutil.rmtree(self.zip_path)
+                syslog.syslog(syslog.LOG_INFO, 'purged local unpacked directory: %s' % self.zip_path)
+
+                self.rmdir(self.zip_path)
+
+            os.unlink(self.fullname)
+            syslog.syslog(syslog.LOG_INFO, 'purged local file: %s' % self.fullname)
+
+            self.rmdir(self.fullname)
+            return True
+        except Exception as e:
+            if isinstance(e, OSError) and e.errno != errno.ENOENT:
+                log_exception(e)
+
+    def unzip(self):
+        try:
+            if self.zip_path:
+                self.mkdir(self.zip_path)
+
+                os.chdir(self.zip_path)
+                subprocess.check_call(self.zip_exec + [self.fullname])
+                syslog.syslog(syslog.LOG_INFO, 'unpacked %s into %s' % (self.fullname, self.zip_path))
         except Exception as e:
             log_exception(e)
 
-        return False
+    def fetch(self, hdfs, temp=tempfile.gettempdir(), skip=False):
+        if skip:
+            return True
+
+        try:
+            with tempfile.NamedTemporaryFile(dir=temp, delete=False) as data:
+                syslog.syslog(syslog.LOG_DEBUG, 'created temp file: %s' %  data.name)
+
+                hdfs.get(self.remote.full, data=data)
+                syslog.syslog(syslog.LOG_DEBUG, 'fetched hdfs file: %s' %  self.remote.full)
+
+            self.mkdir(os.path.dirname(self.fullname))
+
+            os.chmod(data.name, os.stat(data.name).st_mode|stat.S_IRGRP|stat.S_IROTH)
+            os.rename(data.name, self.fullname)
+
+            self.unzip()
+            self.mtime = os.stat(self.fullname).st_mtime
+
+            syslog.syslog(syslog.LOG_DEBUG, 'renamed temp file from %s to %s' % (data.name, self.fullname))
+            return True
+        except Exception as e:
+            log_exception(e)
+            if os.path.exists(data.name):
+                try:
+                    os.remove(data.name)
+                except Exception as e:
+                    log_exception(e)
+
 
 def log_exception(ex):
     syslog.syslog(syslog.LOG_ERR, str(ex))
@@ -108,10 +170,10 @@ def setup_syslog(dest):
 
     syslog.syslog(syslog.LOG_DEBUG, 'logging started')
 
-def setup_socket(port):
+def setup_socket(port, sock=socket.socket()):
     try:
-        sock = socket.socket()
         sock.bind(('127.0.0.1', port))
+        sock.listen(1)
     except socket.error as e:
         if e.errno == errno.EADDRINUSE:
             syslog.syslog(syslog.LOG_ERR, 'another hdfs_sync process is already running')
@@ -119,58 +181,65 @@ def setup_socket(port):
             log_exception(e)
         sys.exit(1)
 
-def setup_local(index):
+def setup_local(index, mirror):
     local = {}
 
+    syslog.syslog(syslog.LOG_DEBUG, 'reading local index: %s' % index)
     try:
-        local = pickle.load(open(index))
-        for key, val in local.items():
-            if not os.path.exists(key) or val.md5 != val.mkmd5():
+        for key, val in pickle.load(open(index)).items():
+            if val.mirror != mirror:
+                val.mirror = mirror
+                syslog.syslog(syslog.LOG_INFO, 'detected sync directory move from %s to %s' % (val.mirror, mirror))
+
+            if not val.modified:
+                local[key] = val
+            else:
                 syslog.syslog(syslog.LOG_INFO, 'file changed or disappeared: %s' % key)
-                del local[key]
     except Exception as e:
         log_exception(e)
 
     syslog.syslog(syslog.LOG_INFO, 'loaded local index containing %d items' % len(local))
     return local
 
-def setup_avail(cmd, url, dst):
+def setup_avail(client, source, mirror, unpack):
     avail = {}
-    lscmd = {'old': 'hadoop fs -lsr', 'new': 'hdfs dfs -ls -R'}[cmd].split()
-    lscmd.append(url)
 
-    syslog.syslog(syslog.LOG_DEBUG, 'running: %s' % ' '.join(lscmd))
-    for line in subprocess.check_output(lscmd, stderr=subprocess.STDOUT).split('\n'):
-        if not line:
+    syslog.syslog(syslog.LOG_DEBUG, 'fetching file list')
+    for item in client.ls(source, recurse=True):
+        if item.name.endswith('_COPYING_'):
             continue
 
         try:
-            item = Sync_File(line, cmd, url, dst)
-            if not item.isdir():
-                syslog.syslog(syslog.LOG_INFO, 'found hdfs item: %s' % item.hdfs.url)
-                avail[item.tgt] = item
+            if not item.is_dir():
+                syslog.syslog(syslog.LOG_INFO, 'queueing hdfs object: %s' % item.full)
+                avail[item.full] = SyncFile(item, source, mirror, unpack)
             else:
-                syslog.syslog(syslog.LOG_DEBUG, 'skipping directory: %s' % item.hdfs.url)
+                syslog.syslog(syslog.LOG_DEBUG, 'skipping directory: %s' % item.full)
         except Exception as e:
             log_exception(e)
 
     syslog.syslog(syslog.LOG_INFO, 'read remote list containing %d items' % len(avail))
     return avail
 
-if __name__ == '__main__':
-    import argparse
 
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='hdfs directory sync')
     parser.add_argument('-u', '--hdfs-url', required=True,
                         help='full hdfs url to sync')
     parser.add_argument('-d', '--dest-dir', required=True,
                         help='destination directory')
-    parser.add_argument('-c', '--hdfs-cmd', default='new', choices=['new', 'old'],
-                        help='hdfs command type')
+    parser.add_argument('-t', '--temp-dir', default='/tmp',
+                        help='where to put the temporary directory for downloads')
+    parser.add_argument('-s', '--sync-dir', default='mirror',
+                        help='relative directory to mirror sources into')
+    parser.add_argument('-e', '--arch-dir', default='unpack',
+                        help='relative directory to unpack archives into')
     parser.add_argument('-p', '--run-port', default=2311, type=int,
-                        help='loopback bind port for mutually exclusive execusion')
+                        help='lock loopback port number')
     parser.add_argument('-l', '--log-dest', choices=['console', 'user', 'daemon'] + ['local%d' % i for i in xrange(8)],
                         help='syslog facility')
+    parser.add_argument('-w', '--workers', type=int, default=multiprocessing.cpu_count(),
+                        help='number of download threads')
     parser.add_argument('-n', '--dry-run', default=False, action='store_true',
                         help='show actions to be performed')
     args = parser.parse_args()
@@ -178,19 +247,37 @@ if __name__ == '__main__':
     setup_syslog(args.log_dest)
     setup_socket(args.run_port)
 
+    dest_dir = args.dest_dir.rstrip('/')
+    temp_dir = args.temp_dir.rstrip('/')
+    sync_dir = '%s/%s' % (dest_dir, args.sync_dir.strip('/'))
+    arch_dir = '%s/%s' % (dest_dir, args.arch_dir.strip('/'))
+
+    hdfs_url = urlparse.urlparse(args.hdfs_url)
+    hdfs_dir = hdfs_url.path
+    hdfs_api = webhdfs.WebHDFSClient(hdfs_url._replace(path='').geturl(), user=getpass.getuser())
+
     try:
-        index = '%s/.%s.pkl' % (args.dest_dir, os.path.splitext(os.path.basename(sys.argv[0]))[0])
-        syslog.syslog(syslog.LOG_DEBUG, 'reading local index: %s' % index)
+        if not os.path.exists(sync_dir):
+            os.makedirs(sync_dir)
+            syslog.syslog(syslog.LOG_INFO, 'created mirror path: %s' % sync_dir)
+        if not os.path.exists(arch_dir):
+            os.makedirs(arch_dir)
+            syslog.syslog(syslog.LOG_INFO, 'created unpack path: %s' % arch_dir)
 
-        spool = tempfile.mkdtemp(dir=args.dest_dir)
-        syslog.syslog(syslog.LOG_DEBUG, 'created work directory: %s' % spool)
-
-        local = setup_local(index)
-        avail = setup_avail(args.hdfs_cmd, args.hdfs_url, args.dest_dir)
+        index = '%s/.%s.idx' % (dest_dir, os.path.splitext(os.path.basename(sys.argv[0]))[0])
+        local = setup_local(index, sync_dir)
+        avail = setup_avail(hdfs_api, hdfs_dir, sync_dir, arch_dir)
+        procs = multiprocessing.pool.ThreadPool(processes=args.workers)
+        xfers = {}
 
         for key, val in avail.items():
-            if (key not in local or not val.equal(local[key])) and val.fetch(spool, args.dry_run):
-                local[key] = val
+            if (key not in local or not val.equal(local[key])):
+                xfers[key] = procs.apply_async(val.fetch, (hdfs_api, temp_dir, args.dry_run))
+        for key, val in xfers.items():
+            if val.get():
+                local[key] = avail[key]
+            else:
+                syslog.syslog(syslog.LOG_ERR, 'failed to fetch %s' % key)
         for key, val in local.items():
             if key not in avail and val.purge(args.dry_run):
                 del(local[key])
@@ -199,9 +286,3 @@ if __name__ == '__main__':
             pickle.dump(local, open(index, 'w'))
     except Exception as e:
         log_exception(e)
-    finally:
-        try:
-            syslog.syslog(syslog.LOG_INFO, 'removing work directory: %s' % spool)
-            shutil.rmtree(spool)
-        except:
-            pass
