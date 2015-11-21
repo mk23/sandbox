d9472095d5fd5c809180653d008ac136342dc114
Max Kalika <mkalika@apple.com>
2015-11-20 17:44:28 -0800

    Standardize placeholders, add issue extractor, add commit range, add custom message.

--- misc/release.py
+++ misc/release.py
@@ -7,6 +7,10 @@ import os
 import re
 import subprocess
 import sys
+import textwrap
+
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
+    pass
 
 def package_info(key, cache={}):
     try:
@@ -19,7 +23,7 @@ def package_info(key, cache={}):
     except (OSError, subprocess.CalledProcessError):
         return ''
 
-def bump_version(bump_major=False, bump_minor=False, bump_patch=False):
+def bump_version(suffix='', bump_major=False, bump_minor=False, bump_patch=False):
     types = {
         'ds': r'(?P<MAJOR>\d{8})\.(?P<PATCH>\d{3})(?P<EXTRA>.*)',
         'mm': r'(?P<MAJOR>\d+)\.(?P<MINOR>\d+)\.(?P<BUILD>\d+)(?:\.(?P<PATCH>\d+))?(?P<EXTRA>.*)',
@@ -60,14 +64,14 @@ def bump_version(bump_major=False, bump_minor=False, bump_patch=False):
 
                 version = v_fmt % tuple(v_arg)
 
-            return version + (match.group('EXTRA') or '')
+            return version + (suffix.replace('_', '-').replace('~', '+') or match.group('EXTRA') or '')
 
     raise RuntimeError('unknown version pattern detected')
 
 def main(argv=sys.argv[1:]):
-    parser = argparse.ArgumentParser(description='debian package release helper', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(description='debian package release helper', formatter_class=CustomFormatter)
     parser.add_argument('-e', '--extra', default=[], nargs=2, action='append', metavar=('FILE', 'REGEX'),
-                        help='extra files to update with specified regex that contain one of {version}, {branch}, or {tag}')
+                        help='extra files to update with specified regex with placeholders')
     if bool(package_info('version')):
         action = parser.add_mutually_exclusive_group()
         action.add_argument('-j', '--major', default=False, action='store_true',
@@ -76,11 +80,15 @@ def main(argv=sys.argv[1:]):
                             help='force increment minor number')
         action.add_argument('-t', '--patch', default=False, action='store_true',
                             help='force increment patch number')
+        parser.add_argument('-m', '--message', default='Tagging {version}',
+                            help='changelog commit message')
         action.add_argument('-v', '--version',
                             help='force explicit version number')
         parser.add_argument('-p', '--package', default=package_info('source'),
                             help='package name')
     else:
+        parser.add_argument('-m', '--message', default='Tagging initial {version}',
+                            help='changelog commit message')
         parser.add_argument('-v', '--version', required=True,
                             help='force explicit version number')
         parser.add_argument('-p', '--package', required=True,
@@ -91,33 +99,55 @@ def main(argv=sys.argv[1:]):
                         help='scm source directories to include in changelog')
     parser.add_argument('-r', '--release', default=package_info('distribution') or 'stable',
                         help='package distribution')
+    parser.add_argument('-x', '--extract', nargs=3, metavar=('REGEX', 'NAME', 'URL'),
+                        help='commit body issue extractor url with {item} placeholder, issue tracker name, and regex pattern')
+    parser.add_argument('-a', '--append', default='',
+                        help='append suffix to version string')
     parser.add_argument('-d', '--no-dch', default=False, action='store_true',
                         help='skip updating debian changelog')
     parser.add_argument('-c', '--commit', default=False, action='store_true',
                         help='commit and tag new changelog')
-    parser.add_argument('-f', '--gittag', default='{package}.{version}',
-                        help='git tag format with {package} and {version} placeholders')
+    parser.add_argument('-g', '--sha1-range', default=['{last_sha1}', 'HEAD'], nargs=2, metavar=('START', 'END'),
+                        help='git commit range with placeholders')
+    parser.add_argument('-f', '--tag-format', default='{package}.{version}',
+                        help='git tag format with placeholders')
+    parser.epilog = textwrap.dedent('''
+        supported placeholders:
+            {this_sha1} : latest commit sha1 hash
+            {last_sha1} : previous release or specified starting commit sha1 hash
+            {tag}       : computed tag string (cannot be used in --tag-format)
+            {branch}    : current checked out branch
+            {package}   : current source package name
+            {version}   : calculated or specified version
+    ''')
     args = parser.parse_args(argv)
 
-    branch  = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip()
-    version = args.version or bump_version(args.major, args.minor, args.patch)
+    strings = {
+        'this_sha1' : subprocess.check_output(['git', 'log', '-1', '--format=%H']).strip(),
+        'branch'    : subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip(),
+        'version'   : package_info('version'),
+        'package'   : args.package or package_info('source'),
+    }
+    strings['last_sha1'] = subprocess.check_output(['git', 'log', '--no-merges', '-1', '--format=%H', args.tag_format.format(**strings)]).strip()
+    strings['version']   = args.version or bump_version(args.append.format(**strings), args.major, args.minor, args.patch)
+    strings['tag']       = args.tag_format.format(**strings)
+
     changed = ['debian/changelog']
 
     if not args.no_dch:
         if bool(package_info('version')):
-            print 'creating changelog entry for %s ...' % version
-            subprocess.check_output(['dch', '--force-bad-version', '--newversion', version, 'Tagging %s' % version])
+            print 'creating changelog entry for %s ...' % strings['version']
+            subprocess.check_output(['dch', '--force-bad-version', '--newversion', strings['version'], args.message.format(**strings)])
 
-            git_tag = args.gittag.format(package=args.package, version=package_info('version'))
-            git_cmd = ['git', 'log', '--oneline', '%s..HEAD' % git_tag]
+            git_cmd = ['git', 'log', '--no-merges', '--format=%h %s (%aN)', '%s..%s' % (args.sha1_range[0].format(**strings), args.sha1_range[1].format(**strings))]
             git_cmd.extend(set(itertools.chain.from_iterable(args.sources)))
 
             changes = subprocess.check_output(git_cmd)
         else:
-            print 'creating new changelog for %s ...' % version
-            subprocess.check_output(['dch', '--create', '--package', args.package, '--newversion', version, 'Tagging initial %s' % version])
+            print 'creating new changelog for %s ...' % strings['version']
+            subprocess.check_output(['dch', '--create', '--package', args.package, '--newversion', strings['version'], args.message.format(**strings)])
 
-            changes = subprocess.check_output(['git', 'log', '--oneline']) if not args.skiplog else ''
+            changes = subprocess.check_output(['git', 'log', '--no-merges', '--format=%h %s (%aN)']) if not args.skiplog else ''
 
         for line in reversed(changes.strip().split('\n')[::-1]):
             if not line:
@@ -128,27 +158,32 @@ def main(argv=sys.argv[1:]):
             print '\tappending changelog message for %s ...' % sha1
             subprocess.check_output(['dch', '--append', '[%s] %s' % (sha1, text)])
 
-        print 'finalizing changelog release for %s ...' % version
+            if args.extract:
+                text = subprocess.check_output(['git', 'show', '-s', '--format=%b', sha1])
+                bugs = re.findall('%s' % args.extract[0], text, re.IGNORECASE)
+                if bugs:
+                    subprocess.check_output(['dch', '--append', '  %s%s addressed:' % (args.extract[1].capitalize(), 's' if len(bugs) != 1 else '')])
+                    for item in bugs:
+                        subprocess.check_output(['dch', '--append', '    ' + args.extract[2].format(item=item)])
+
+
+        print 'finalizing changelog release for %s ...' % strings['version']
         subprocess.check_output(['dch', '--release', '--distribution', args.release, ''])
 
     for name, patt in args.extra:
         print 'checking %s ...' % name
-        part = '(?P<PATTERN>%s)' % patt.format(version='(?P<VERSION>.*?)', branch='(?P<BRANCH>.*?)', tag='(?P<TAG>.*?)')
+        part = '(?P<PATTERN>%s)' % patt.format(**dict( (i, '(?P<%s>.*?)' % i) for i in strings.keys()))
         text = open(name).read()
         find = re.search(part, text).groupdict()
         if find:
             print '\tmodifying %s ...' % name
-            if 'VERSION' in find:
-                text = text.replace(find['PATTERN'], find['PATTERN'].replace(find['VERSION'], version))
-            if 'BRANCH' in find:
-                text = text.replace(find['PATTERN'], find['PATTERN'].replace(find['BRANCH'], branch))
-            if 'TAG' in find:
-                text = text.replace(find['PATTERN'], find['PATTERN'].replace(find['TAG'], '%s.%s' % (args.package, version)))
+            for key, val in find.items():
+                if key in strings:
+                    text = text.replace(find['PATTERN'], find['PATTERN'].replace(find[key], strings[key]))
 
-            data = open(name, 'w')
-            data.write(text)
-            data.close()
-            changed.append(name)
+            with open(name, 'w') as data:
+                data.write(text)
+                changed.append(name)
 
     if args.commit:
         print 'updating git ...'
@@ -156,9 +191,9 @@ def main(argv=sys.argv[1:]):
             print '\tadding changed file to git %s ...' % name
             subprocess.check_output(['git', 'add', name])
         print '\tcommitting changelog to git ...'
-        subprocess.check_output(['git', 'commit', '-m', 'Tagging %s' % version] + changed)
+        subprocess.check_output(['git', 'commit', '-m', args.message.format(**strings)] + changed)
         print '\ttagging changelog in git ...'
-        subprocess.check_output(['git', 'tag', args.gittag.format(package=args.package, version=version)])
+        subprocess.check_output(['git', 'tag', args.tag_format.format(**strings)])
         print 'release prep complete, verify and push the changes and tag'
 
 if __name__ == '__main__':
